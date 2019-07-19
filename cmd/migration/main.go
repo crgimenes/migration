78b64fbb07956c3c3008676c5ee0950739a342aa
Felipe Oliveira <fpo@felipeweb.dev>
2019-07-19 15:09:10 -0300

    added cloud drivers
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gosidekick/migration/v2/cmd"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
+	_ "gocloud.dev/postgres/awspostgres"
+	_ "gocloud.dev/postgres/gcppostgres"
 )
 
 func main() {
